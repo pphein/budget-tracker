8d744a704f6f7a28c88dbca7e24067ba6ea7ca2d
pph <pph>
2025-03-26 17:52:02 +0630

    Chnaged ui width

--- src/App.js
+++ src/App.js
@@ -163,165 +163,139 @@ const App = () => {
     console.log("filteredRecords >> ", filteredRecords);
     console.log("Tags for app >> ", tags);
     return (
-        <div className="w-container p-6 flex justify-center flex-col items-center">
-            <div className="flex justify-between border-b bg-gray-100 px-10">
-              <button
-                  className={`py-2 px-16 ${activeTab === 'income' ? 'border-b-2 border-blue-500' : ''}`}
-                  onClick={() => handleTabChange('income')}
-              >
-                  Income
-              </button>
-              <button
-                  className={`py-2 px-16 ${activeTab === 'expense' ? 'border-b-2 border-blue-500' : ''}`}
-                  onClick={() => handleTabChange('expense')}
-              >
-                  Expense
-              </button>
-        </div>
-            <div className="my-4 flex flex-row">
-                <h2 className="text-xl font-bold my-2 mx-2">New Tag: </h2>
-                <div className="flex space-x-2">
-                    <input
-                        type="text"
-                        value={newTag}
-                        onChange={(e) => setNewTag(e.target.value)}
-                        placeholder="Add custom tag"
-                        className="border px-2 py-1 rounded"
-                    />
-                    <button onClick={createTag} className="px-4 py-2 bg-blue-500 text-white rounded">
-                        Add
-                    </button>
-                </div>
-            </div>
-
-            {/* Transaction Form */}
-            <div className="space-3 mb-3">
-              <div className='flex space-x-2 flex-row justify-between items-end'>
-                <div style={{display: "none"}}>
-                  <label className="block">Type:</label>
-                  <select value={type} onChange={(e) => setType(e.target.value)} className="p-2 border rounded">
-                    <option value={tab}>{tab}</option>
-                  </select>
-                </div>
-                <div>
-                  <label className="block">Tag:</label>
-                  <select value={tag} onChange={(e) => setTag(e.target.value)} className="p-2 border rounded">
-                    {tags.map((t) => (
-                        <option key={t.id} value={t.name}> {t.name}</option>                        
-                      ))
-                    }
-                  </select>
-                </div>
-                <div>
-                  <label className="block">Date:</label>
-                  {/* <input type="date" value={date} onChange={(e) => setDate(e.target.value)} className="w-full p-2 border rounded"/> */}
-                  <DatePicker selected={date} onChange={(date) => setDate(formatDateTime(date))} dateFormat="dd-MM-YYYY" showIcon className="w-32 p-2 border rounded" />
-                </div>
-                <div>
-                  <label className="block">Amount:</label>
-                  <input
-                    type="text"
-                    value={amount}
-                    onChange={(e) => {
-                      const input = e.target.value;
-                      if (/^[0-9+\s]*$/.test(input)) {
-                        setAmount(input);
-                      }
-                    }}
-                    onBlur={(e) => {
-                      try {
-                        const result = eval(e.target.value.replace(/\s+/g, ''));
-                        if (!isNaN(result)) {
-                          setAmount(result.toString());
-                        }
-                      } catch {
-                        alert("Invalid expression");
-                      }
-                    }}
-                    className="w-24 p-2 border rounded"
-                  />
-                </div>
-                <div>
-                  <button onClick={handleAddTransaction} type="submit" className={`w-full ${tab === 'income' ? 'bg-green-500' : 'bg-red-500'} text-white p-2 rounded`}>
-                  Save
+        <div className="w-container m-auto flex flex-row justify-center items-center">
+           <div style={{marginTop: "450px"}} className='absolute xs:w-32 flex flex-row justify-center flex-col items-center flex-wrap'>
+            <div className="flex mt- justify-between border-b bg-gray-100 px-10">
+                <button
+                    className={`py-2 px-16 ${activeTab === 'income' ? 'border-b-2 border-blue-500' : ''}`}
+                    onClick={() => handleTabChange('income')}
+                >
+                    Income
                 </button>
-                </div>
+                <button
+                    className={`py-2 px-16 ${activeTab === 'expense' ? 'border-b-2 border-blue-500' : ''}`}
+                    onClick={() => handleTabChange('expense')}
+                >
+                    Expense
+                </button>
+              </div>
+              <div className="my-4 flex flex-row">
+                  <h2 className="text-xl font-bold my-2 mx-2">New Tag: </h2>
+                  <div className="flex space-x-2">
+                      <input
+                          type="text"
+                          value={newTag}
+                          onChange={(e) => setNewTag(e.target.value)}
+                          placeholder="Add custom tag"
+                          className="border px-2 py-1 rounded"
+                      />
+                      <button onClick={createTag} className="px-4 py-2 bg-blue-500 text-white rounded">
+                          Add
+                      </button>
+                  </div>
               </div>
 
-              {/* <div className='flex space-x-2 flex-row justify-between items-end'>
-                <div>
-                  <label className="block">Amount:</label>
-                  <input
-                    type="text"
-                    value={amount}
-                    onChange={(e) => {
-                      const input = e.target.value;
-                      if (/^[0-9+\s]*$/.test(input)) {
-                        setAmount(input);
+              {/* Transaction Form */}
+              <div className="space-3 mb-3">
+                <div className='flex space-x-2 flex-row justify-between items-end'>
+                  <div style={{display: "none"}}>
+                    <label className="block">Type:</label>
+                    <select value={type} onChange={(e) => setType(e.target.value)} className="p-2 border rounded">
+                      <option value={tab}>{tab}</option>
+                    </select>
+                  </div>
+                  <div>
+                    <label className="block">Tag:</label>
+                    <select value={tag} onChange={(e) => setTag(e.target.value)} className="p-2 border rounded">
+                      {tags.map((t) => (
+                          <option key={t.id} value={t.name}> {t.name}</option>                        
+                        ))
                       }
-                    }}
-                    onBlur={(e) => {
-                      try {
-                        const result = eval(e.target.value.replace(/\s+/g, ''));
-                        if (!isNaN(result)) {
-                          setAmount(result.toString());
+                    </select>
+                  </div>
+                  <div>
+                    <label className="block">Date:</label>
+                    {/* <input type="date" value={date} onChange={(e) => setDate(e.target.value)} className="w-full p-2 border rounded"/> */}
+                    <DatePicker selected={date} onChange={(date) => setDate(formatDateTime(date))} dateFormat="dd-MM-YYYY" showIcon className="w-32 p-2 border rounded" />
+                  </div>
+                  <div>
+                    <label className="block">Amount:</label>
+                    <input
+                      type="text"
+                      value={amount}
+                      onChange={(e) => {
+                        const input = e.target.value;
+                        if (/^[0-9+\s]*$/.test(input)) {
+                          setAmount(input);
                         }
-                      } catch {
-                        alert("Invalid expression");
-                      }
-                    }}
-                    className="w-full p-2 border rounded"
-                  />
+                      }}
+                      onBlur={(e) => {
+                        try {
+                          const result = eval(e.target.value.replace(/\s+/g, ''));
+                          if (!isNaN(result)) {
+                            setAmount(result.toString());
+                          }
+                        } catch {
+                          alert("Invalid expression");
+                        }
+                      }}
+                      className="w-24 p-2 border rounded"
+                    />
+                  </div>
+                  <div>
+                    <button onClick={handleAddTransaction} type="submit" className={`w-full ${tab === 'income' ? 'bg-green-500' : 'bg-red-500'} text-white p-2 rounded`}>
+                    Save
+                  </button>
+                  </div>
                 </div>
-                <div>
-                  <button onClick={handleAddTransaction} type="submit" className={`w-full ${tab === 'income' ? 'bg-green-500' : 'bg-red-500'} text-white p-2 rounded`}>
-                  Save
-                </button>
-                </div>
-              </div> */}
-            </div>
-            <div className='bg-gray-200 p-4 mt-4'>
-              <Filter
-                  tags={tags}
-                  selectedTag={selectedTag}
-                  setSelectedTag={setSelectedTag}
-                  startDate={startDate}
-                  setStartDate={setStartDate}
-                  endDate={formatDateTime(endDate)}
-                  setEndDate={setEndDate}
-                  formatDateTime={formatDateTime}
-              />
 
-              <RecordList className="mt-16" type={type} records={filteredRecords} handleDeleteTransaction={handleDeleteTransaction} handleEditTransaction={handleEditTransaction} formatDateTime={formatDateTime} />
-            </div>
-            {/* <div className="my-4">
-              <button
-                onClick={() => {
-                  const headers = ["Type", "Tag", "Amount", "Date"];
-                  const rows = filteredRecords.map((record) => [
-                    record.type,
-                    record.tag,
-                    record.amount,
-                    new Date(record.date).toLocaleString(),
-                  ]);
+                {/* <div className='flex space-x-2 flex-row justify-between items-end'>
+                  <div>
+                    <label className="block">Amount:</label>
+                    <input
+                      type="text"
+                      value={amount}
+                      onChange={(e) => {
+                        const input = e.target.value;
+                        if (/^[0-9+\s]*$/.test(input)) {
+                          setAmount(input);
+                        }
+                      }}
+                      onBlur={(e) => {
+                        try {
+                          const result = eval(e.target.value.replace(/\s+/g, ''));
+                          if (!isNaN(result)) {
+                            setAmount(result.toString());
+                          }
+                        } catch {
+                          alert("Invalid expression");
+                        }
+                      }}
+                      className="w-full p-2 border rounded"
+                    />
+                  </div>
+                  <div>
+                    <button onClick={handleAddTransaction} type="submit" className={`w-full ${tab === 'income' ? 'bg-green-500' : 'bg-red-500'} text-white p-2 rounded`}>
+                    Save
+                  </button>
+                  </div>
+                </div> */}
+              </div>
+              <div className='bg-gray-200 p-4 mt-4'>
+                <Filter
+                    tags={tags}
+                    selectedTag={selectedTag}
+                    setSelectedTag={setSelectedTag}
+                    startDate={startDate}
+                    setStartDate={setStartDate}
+                    endDate={formatDateTime(endDate)}
+                    setEndDate={setEndDate}
+                    formatDateTime={formatDateTime}
+                />
 
-                  const csvContent =
-                    "data:text/csv;charset=utf-8," +
-                    [headers.join(","), ...rows.map((row) => row.join(","))].join("\n");
-
-                  const encodedUri = encodeURI(csvContent);
-                  const link = document.createElement("a");
-                  link.setAttribute("href", encodedUri);
-                  link.setAttribute("download", "transactions.csv");
-                  document.body.appendChild(link);
-                  link.click();
-                  document.body.removeChild(link);
-                }}
-                className="px-4 py-2 bg-green-500 text-white rounded"
-              >
-                Export to Excel
-              </button>
-            </div> */}
+                <RecordList className="mt-16" type={type} records={filteredRecords} handleDeleteTransaction={handleDeleteTransaction} handleEditTransaction={handleEditTransaction} formatDateTime={formatDateTime} />
+              </div>
+           </div>
         </div>
     );
 };
